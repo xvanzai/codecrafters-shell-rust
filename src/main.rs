4f00675e488f8e4308f54ca1cc784718421d1a22
xvanzai <xvanzai@xvanzaideMacBook-Air.local>
2026-05-03 19:53:13 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+const COMMAND_EXIT: &str = "exit";
+
 fn main() {
     loop {
         print!("$ ");
@@ -8,6 +10,14 @@ fn main() {
 
         let mut user_command = String::new();
         io::stdin().read_line(&mut user_command).unwrap();
-        println!("{}: command not found", user_command.trim())
+
+        match user_command.trim() {
+            COMMAND_EXIT => {
+                break;
+            }
+            _ => {
+                println!("{}: command not found", user_command.trim())
+            }
+        }
     }
 }
