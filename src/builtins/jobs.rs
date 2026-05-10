c3858b2b7a96157d0239fa0f5daec2fa9b532ce1
xvanzai <xvanzai175@gmail.com>
2026-05-10 19:57:42 +0800

    feat: extend the jobs builtin to list multiple background jobs. [skip ci]

--- src/builtins/jobs.rs
+++ src/builtins/jobs.rs
@@ -1,7 +1,7 @@
 use std::io::Write;
 
 use crate::{
-    builtins::{Builtin, ShouldExit},
+    builtins::{Builtin, ShouldExit, jobs},
     context::ShellContext,
     error::ShellError,
 };
@@ -19,8 +19,20 @@ impl Builtin for JobsBuiltin {
         context: &mut ShellContext,
         writer: &mut dyn Write,
     ) -> Result<ShouldExit, ShellError> {
-        for job in context.list_background_jobs() {
-            writeln!(writer, "[{}]+  {:<24}{} &", job.id, "Running", job.command)?;
+        let jobs = context.list_background_jobs();
+        for (i, job) in jobs.iter().enumerate() {
+            let marker = if i == jobs.len() - 1 {
+                "+"
+            } else if i == jobs.len() - 2 {
+                "-"
+            } else {
+                " "
+            };
+            writeln!(
+                writer,
+                "[{}]{}  {:<24}{} &",
+                job.id, marker, "Running", job.command
+            )?;
         }
         Ok(ShouldExit::Continue)
     }
@@ -30,6 +42,6 @@ use std::process::Child;
 
 pub struct Job {
     pub id: usize,
-    pub command: String,   // 重建的命令文本
+    pub command: String, // 重建的命令文本
     pub child: Child,
 }
